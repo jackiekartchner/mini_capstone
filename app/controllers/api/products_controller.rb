723e893e2891552c527be1718c843eee1794703d
jackie <jacqueline.kartchner@gmail.com>
2019-08-28 10:56:10 -0700

    adds each individual prodcut route to mini_capstone

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,7 +1,22 @@
 class Api::ProductsController < ApplicationController
 
-  def all_products
+  def all_products_method
     @products = Product.all
     render 'all_products.json.jb'
+  end
+
+  def first_product_method
+    @product = Product.first 
+    render 'first_product.json.jb'
+  end
+
+  def third_product_method
+    @product = Product.third 
+    render 'third_product.json.jb'
+  end
+
+  def fourth_product_method
+    @product = Product.fourth 
+    render 'fourth_product.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "all_products/_url" => "productss#all_products_method"
+    get "/all_products_url" => "products#all_products_method"
+    get "/first_product_url" => "products#first_product_method"
+    get "/second_product_url" => "products#second_product_method"
+    get "/third_product_url" => "products#third_product_method"
+    get "/fourth_product_url" => "products#fourth_product_method"
   end
 end
